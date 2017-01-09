eb7fa7895c9cc57317251239dad7fea575d65be6
Tom Liversidge <tom@liversidge.me>
2017-01-09 12:14:26 +0200

    add color pick

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,12 +1,20 @@
 defmodule Identicon do
+
   def generate(input) do
-      input
-      |> hash
+    input
+    |> hash
+    |> pick_color
   end
 
-  defp hash(input) do
-    :crypto.hash(:md5, input)
+  def hash(input) do
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
 end
--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -3,6 +3,13 @@ defmodule IdenticonTest do
   doctest Identicon
 
   test "string to number array" do
-    assert Identicon.generate("hello world") == [94, 182, 59, 187, 224, 30, 238, 208, 147, 203, 34, 187, 143, 90, 205, 195]
+    assert Identicon.hash("hello world") ==
+      %Identicon.Image{hex: [94, 182, 59, 187, 224, 30, 238, 208, 147, 203,
+             34, 187, 143, 90, 205, 195]}
+  end
+
+  test "color pick takes first 3 items from hex as RGB" do
+    assert Identicon.pick_color(%Identicon.Image{hex: [1,2,3,4,5]}) ==
+      %Identicon.Image{color: {1,2,3}, hex: [1,2,3,4,5]}
   end
 end
